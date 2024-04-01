c9ac850b8633d7022c9d9bdece6daf84275d3a63
Ashutosh Sharma <ashutoshsharma18122004@gmail.com>
2024-04-01 23:09:02 +0530

    server creation

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app=  express();
+const port = 8080;
+
+app.get("/", (req, res)=>{
+    res.send(" This is home route");
+
+})
+
+app.listen(port, ()=>{
+    console.log(`app is listening on port ${port}`);
+});
