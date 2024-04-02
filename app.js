b3febfd8eb9cc64660d6c84ea37196f1f9beb37f
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-04-03 01:25:46 +0530

    sellerapp

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=  express();
 const port = 8080;
 const mongoose = require('mongoose');
 const Seller= require("./models/seller.js");
+const sellerApp=require("./sellerapp.js");
 
 const dbUrl="mongodb://127.0.0.1:27017/hawker"
 main()
@@ -21,24 +22,10 @@ async function main() {
 app.get("/", (req, res)=>{
     res.send(" This is home route");
 
-})
-app.get("/testseller",async(req,res)=>{
-    let sampleSeller=new Seller({
-        title:"Green Vegetable",
-        name:"Onion",
-        quantity:50,
-        // image:"",
-        description:"eat healthy vegetable",
-        price:25,
-        location:"Gorakhpur",
-        country:"India"
-       
+});
+app.use(sellerApp);
 
-    });
-    await sampleSeller.save();
-    console.log("sample was saved");
-      res.send("successfull  testing");
-})
+
 
 app.listen(port, ()=>{
     console.log(`app is listening on port ${port}`);
