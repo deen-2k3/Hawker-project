7220d6d3947cc5393b60a5f65bea39a51d8adc21
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-04-03 01:08:48 +0530

    database initalize

--- app.js
+++ app.js
@@ -2,12 +2,9 @@ const express = require("express");
 const app=  express();
 const port = 8080;
 const mongoose = require('mongoose');
+const Seller= require("./models/seller.js");
 
 const dbUrl="mongodb://127.0.0.1:27017/hawker"
-
-async function main() {
-    await mongoose.connect(dbUrl);
-}
 main()
 .then(()=>{
     console.log("connect to DB");
@@ -15,11 +12,32 @@ main()
 .catch(err=>{
     console.log(err);
 })
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+
 
 
 app.get("/", (req, res)=>{
     res.send(" This is home route");
 
+})
+app.get("/testseller",async(req,res)=>{
+    let sampleSeller=new Seller({
+        title:"Green Vegetable",
+        name:"Onion",
+        quantity:50,
+        // image:"",
+        description:"eat healthy vegetable",
+        price:25,
+        location:"Gorakhpur",
+        country:"India"
+       
+
+    });
+    await sampleSeller.save();
+    console.log("sample was saved");
+      res.send("successfull  testing");
 })
 
 app.listen(port, ()=>{
