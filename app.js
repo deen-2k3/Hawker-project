172244b3256c2acf3dd12c04c4a55b30e3427596
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-04-02 10:51:49 +0530

    dbs connection

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const app=  express();
 const port = 8080;
+const mongoose = require('mongoose');
+
+const dbUrl="mongodb://127.0.0.1:27017/hawker"
+
+async function main() {
+    await mongoose.connect(dbUrl);
+}
+main()
+.then(()=>{
+    console.log("connect to DB");
+})
+.catch(err=>{
+    console.log(err);
+})
+
 
 app.get("/", (req, res)=>{
     res.send(" This is home route");
