69f7f022f5da6fcfc5701a851d9af4a2e53d4997
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-04-08 09:53:58 +0530

    frontend server

---
+++ Frontend/seller/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Search from './components/login.jsx'
+
+function App() {
+  
+
+  return (
+    <>
+     <Search/>
+    </>
+  )
+}
+
+export default App
