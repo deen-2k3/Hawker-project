c228b99ee530e53dc45dd1a049264828808c5ee0
Ashutosh Sharma <ashutoshsharma18122004@gmail.com>
2024-04-12 23:39:41 +0530

    customerHome

--- Frontend/seller/src/Data/cartData.js
+++ Frontend/seller/src/Data/cartData.js
@@ -1,23 +1,27 @@
 const vegetablesData = [
     {
+      "id" : 1,
       "image": "https://images.unsplash.com/photo-1582284540020-8acbe03f4924?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "name": "Tomato",
       "price": 100,
       "amount": 50
     },
     {
+      "id" : 2,
       "image": "https://images.unsplash.com/photo-1590868309235-ea34bed7bd7f?q=80&w=1854&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "name": "Carrot",
       "price": 80,
       "amount": 30
     },
     {
+      "id" : 3,
       "image": "https://images.unsplash.com/photo-1518977676601-b53f82aba655?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "name": "Potato",
       "price": 150,
       "amount": 20
     },
     {
+      "id" : 4,
       "image": "https://images.unsplash.com/photo-1618512496248-a07fe83aa8cb?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       "name": "Onion",
       "price": 60,
