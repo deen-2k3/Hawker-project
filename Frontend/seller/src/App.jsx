157f4fb2349ab5a3013534179a77bb908ac26564
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-04-13 00:44:29 +0530

    hawker register

--- Frontend/seller/src/App.jsx
+++ Frontend/seller/src/App.jsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Login from './components/login.jsx';
+import Login from './components/Login.jsx';
 import Register from './components/Register.jsx'
 import {BrowserRouter,Routes,Route}  from 'react-router-dom'
 import Homenavbar from './components/Homenavbar.jsx';
@@ -12,7 +12,7 @@ import Homenavbar from './components/Homenavbar.jsx';
 import Cart from './components/Cart.jsx';
 
 import AddHawkerHome from './pages/AddHawkerHome.jsx';
-import servertest from './servertest.jsx';
+import HawkerRegister from './pages/HawkerRegister.jsx';
 
 
 
@@ -25,7 +25,7 @@ function App() {
     <Routes>
     <Route path='/' element={<Homenavbar/>}></Route>
     <Route path='/addhawkerhome' element={<AddHawkerHome/>}></Route>
-
+    <Route path='/hawkerregister' element={<HawkerRegister/>}></Route>
       <Route path='/login' element={<Login/>}></Route>
       <Route path='/register' element={<Register/>}></Route>
       <Route path='/cart' element={<Cart/>}></Route>
