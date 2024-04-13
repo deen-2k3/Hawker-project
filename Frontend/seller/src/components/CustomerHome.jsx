93ef95cd376ddc664624af23966b4670eea87b7a
Ashutosh Sharma <ashutoshsharma18122004@gmail.com>
2024-04-13 23:34:58 +0530

    customerHome

--- Frontend/seller/src/components/CustomerHome.jsx
+++ Frontend/seller/src/components/CustomerHome.jsx
@@ -1,16 +1,29 @@
 import '../styles/customerHome.css';
+import sellerData from '../Data/customerHomedata';
+import { useState, useEffect } from 'react';
+import Homenavbar from './Homenavbar.jsx'
 export default function CustomerHome() {
+    const [items, setItems] = useState([]);
+    useEffect(() => {
+        setItems(sellerData)
+    }, [])
     return (
         <>
-        <div className="container">
-          <div className="card mt-3 " style={{"width": "18rem", "height":"18rem"}}>
-                    <img src="https://images.unsplash.com/photo-1582284540020-8acbe03f4924?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">Tomato</h5>
-                        <p className="card-text">By person</p>
-                        {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+        {/* <Homenavbar/> */}
+        <div className="row row-cols-lg-5 row-cols-md-3 row-cols-sm-1" style={{"margin":"auto"}}>
+                {items.map((item) => (
+                    <div className="card col"  >
+                    <img className="card-img-top" src={item.image} alt="Card image cap" style={{"height":"10rem"}}/>
+                    <div className="card-body" style={{"padding":"0"}}>
+                      <p className="card-text">
+                        {item.name} <br />
+                        by- {item.title} <br />
+                        RS- {item.price} <br />
+                        <b>{item.location}, {item.country} </b>
+                      </p>
                     </div>
-                </div>
+                  </div>
+                ))}
                 </div>
         </>
     )
