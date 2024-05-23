6240c7c8f99bbf102bfac3284ad75aedd574f6d5
Ashutosh Sharma <ashutoshsharma18122004@gmail.com>
2024-05-23 23:54:15 +0530

    order update

--- models/User.js
+++ models/User.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose')
-const Cart = require("./cart.js");
+const Cart = require("cart.js");
 const UserSchema =new mongoose.Schema({
     name:String,
     email:String,
     password:String
-    // cart: { type: Schema.Types.ObjectId, ref: 'Cart' }
+    cart: { type: Schema.Types.ObjectId, ref: Cart }
 })
 
 const  UserModel= mongoose.model("users",UserSchema)
