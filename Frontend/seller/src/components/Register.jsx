448ed14417df8e05c4e01ccbc876e9ddb503db38
Deen Bandhu Singh <deenbandhusingh335@gmail.com>
2024-06-12 17:55:39 +0530

    navigation

--- Frontend/seller/src/components/Register.jsx
+++ Frontend/seller/src/components/Register.jsx
@@ -2,17 +2,25 @@
 
 import { useState } from "react";
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function Register(){
   const [name,setName]=useState()
   const [email,setEmail]=useState()
   const [password,setPassword]=useState()
-  
+  const navigate = useNavigate();
   const handleSubmit=(e)=>{
     e.preventDefault()
     axios.post('http://localhost:8080/signup',{name,email,password})
-    .then(result=>console.log(result))
-    .catch(err=>console.log(err));
+    .then(result => {
+      console.log(result);
+      if (result.status=== 200) {
+        navigate('/'); // Navigate to the protected route
+      }
+    })
+    .catch(err => console.log(err));
   }
     return (
       
--- Frontend/seller/src/pages/HawkerRegister.jsx
+++ Frontend/seller/src/pages/HawkerRegister.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NewHawkerDetails from '../components/NewHawkerDetails'
+import axios from 'axios'
 
 
 export default function HawkerRegister() {
